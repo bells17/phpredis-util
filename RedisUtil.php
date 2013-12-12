8bad3f3400e6dc13a44182dfc366546c7ba345c1
hayakawa <bells171@gmail.com>
2013-12-12 13:45:51 +0900

    change

--- RedisUtil.php
+++ RedisUtil.php
@@ -1,37 +1,24 @@
 <?php
 require_once(dirname(__FILE__) . '/RedisConnectionCollecter.php');
 /**
- * RedisManager
+ * RedisUtil
  * use phpredis
  * https://github.com/nicolasff/phpredis
  * @author hayakawa
  */
-class RedisManager {
+class RedisUtil {
 
 	protected $_delemiter_base 		= ':'; //baseキーの区切り文字
 	protected $_delemiter_key_value = '='; ////キーバリューの区切り文字
 
 
-	/* ---------- redis connection ---------- */
-
-
 	/**
 	 * インスタンス生成時にself::DEFAULT_DBで設定したキー名のredisの接続済みインスタンスをセットします
 	 */
-	public function __construct($dbs, $defaultRedisKey, $delemiter = null)
+	public function __construct($delemiter = null)
 	{
-		$this->setConnection($dbs, $defaultRedisKey);
+		$this->setConnection($dbs);
 		$this->setDelemiter($delemiter);
-	}
-
-	/**
-	 * $this->_redisに指定したキー名でredisの接続済みインスタンスをセットします
-	 */
-	public function setConnection(array $dbs, $key)
-	{
-		foreach ($dbs as $key => $db) {
-			RedisConnectionCollecter::setDbInfo(array($key => $db));
-		}
 	}
 
 	/**
@@ -48,18 +35,6 @@ class RedisManager {
 	}
 
 	/**
-	 * getConnection
-	 */
-	public function getConnection($key)
-	{
-		return RedisConnectionCollecter::getConnection($key);
-	}
-
-
-	/* ---------- redisキーの生成用 ---------- */
-
-
-	/**
 	 * redisのベースになるキー名を生成します
 	 * @param $base_key_args 	ベースとなるキー名を配列で指定してください
 	 * array(
@@ -72,7 +47,7 @@ class RedisManager {
 	 * 		'key2' => 'value2',
 	 * );
 	 *
-	 * @return sample. 'prefix:basekey1:basekey2:key1=value1:key2=value2'
+	 * @return sample. 'basekey1:basekey2:key1=value1:key2=value2'
 	 */
 	public function createRedisKey(array $base_key_args, array $key_value_args)
 	{
